ba414635558d6700d956890b8c428f3036f09d11
redpois0n <red@cmail.nu>
2015-07-21 01:06:04 +0200

    writes array to file.jar

--- src/main.c
+++ src/main.c
@@ -1,4 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define DEBUG true
+
+const char jar[] = { /* array */ };
 
 int main(void) {
-	return 0;
+	FILE *file = fopen("file.jar", "w");
+
+	int results = fputs(jar, file);
+
+	fclose(file);
+
+	return results;
 }
