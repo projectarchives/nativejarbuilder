c3c80516bfe5e20eeab0909762215788c042f449
redpois0n <red@cmail.nu>
2015-07-21 01:11:44 +0200

    filename define

--- src/main.c
+++ src/main.c
@@ -2,11 +2,12 @@
 #include <stdbool.h>
 
 #define DEBUG true
+#define FILENAME "file.jar"
 
 const char jar[] = { /* array */ };
 
 int main(void) {
-	FILE *file = fopen("file.jar", "w");
+	FILE *file = fopen(FILENAME, "w");
 
 	int results = fputs(jar, file);
 
