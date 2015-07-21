9fbe63c2bac6790636af13cc3f96d3a8d43248df
redpois0n <red@cmail.nu>
2015-07-21 15:00:35 +0100

    -f, --file argument

--- gen.py
+++ gen.py
@@ -1,2 +1,18 @@
+import argparse
+
+argparse = argparse.ArgumentParser()
+argparse.add_argument("-f", "--file", dest="file", help="File to parse")
+args = argparse.parse_args()
+
+global file
+file = args.file
+
 if __name__ == "__main__":
-    pass
+    
+    with open(file, "rb") as f:
+        while True:
+            byte =  f.read(1);
+            if not byte:
+                break
+            
+            char = chr(int(byte, 16))
